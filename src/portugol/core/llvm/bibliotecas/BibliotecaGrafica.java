442e62ac34959f59afe39dbf3ebc0c08dc93a413
Bernardo Oliveira Bruning <bernardo.bruning@gmail.com>
2016-08-26 23:38:07 -0300

    :+1: Inclui conjunto de bibliotecas

---
+++ src/portugol/core/llvm/bibliotecas/BibliotecaGrafica.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package portugol.core.llvm.bibliotecas;
+
+import org.llvm.Module;
+import portugol.core.llvm.Biblioteca;
+
+/**
+ *
+ * @author Bernardo
+ */
+public class BibliotecaGrafica implements Biblioteca {
+
+    @Override
+    public void inicializar(Module modulo) {
+        //TODO: Implementar
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+    
+}
