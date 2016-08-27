a730fa78bb86d707b5b6055cc78d26beacc6a780
Bernardo Oliveira Bruning <bernardo.bruning@gmail.com>
2016-08-27 00:22:54 -0300

    :bug: Ajusta erros

--- src/portugol/core/llvm/bibliotecas/BibliotecaEntedaSaida.java
+++ src/portugol/core/llvm/bibliotecas/BibliotecaEntedaSaida.java
@@ -17,7 +17,7 @@ public class BibliotecaEntedaSaida implements Biblioteca {
 
     @Override
     public void inicializar(Module modulo) {
-        this.module.addFunction("escreva", TypeRef.functionType(TypeRef.int32Type(), true, TypeRef.int8Type().pointerType()));
+        module.addFunction("escreva", TypeRef.functionType(TypeRef.int32Type(), true, TypeRef.int8Type().pointerType()));
         //TODO: Implementar saída
     }
     
